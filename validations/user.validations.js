47ad82f9154251caefe7882477fbe5a3daa2f28b
gauravMishra <gauravmishra99696@gmail.com>
2025-06-20 10:55:51 +0530

    new fix

--- validations/user.validations.js
+++ validations/user.validations.js
@@ -1,60 +1,60 @@
-import { object, string } from "joi";
+import joi from "joi";
 
 function validateRegisterInput(input) {
-  const schema = object({
-    userName: string().max(50),
-    password: string().max(255).required(),
-    email: string().max(255).required().email(),
+  const schema = joi.object({
+    userName: joi.string().max(50),
+    password: joi.string().max(255).required(),
+    email: joi.string().max(255).required().email(),
   });
 
   return schema.validate(input);
 }
 
 function validateEmail(input) {
-  const schema = object({
-    email: string().max(255).required().email(),
+  const schema = joi.object({
+    email: joi.string().max(255).required().email(),
   });
 
   return schema.validate(input);
 }
 
 function validatePassword(input) {
-  const schema = object({
-    password: string().max(255).required(),
+  const schema = joi.object({
+    password: joi.string().max(255).required(),
   });
 
   return schema.validate(input);
 }
 
 function validateRegisterEmail(input) {
-  const schema = object({
-    email: string().max(255).required().email(),
-    OTP: string().min(4).max(4).required(),
+  const schema = joi.object({
+    email: joi.string().max(255).required().email(),
+    OTP: joi.string().min(4).max(4).required(),
   });
 
   return schema.validate(input);
 }
 
 function validateLoginInput(input) {
-  const schema = object({
-    password: string().max(255).required(),
-    email: string().max(50).required(),
+  const schema = joi.object({
+    password: joi.string().max(255).required(),
+    email: joi.string().max(50).required(),
   });
 
   return schema.validate(input);
 }
 
 function validateUserName(input) {
-  const schema = object({
-    userName: string().max(50),
+  const schema = joi.object({
+    userName: joi.string().max(50),
   });
 
   return schema.validate(input);
 }
 
 function validateUserMsg(input) {
-  const schema = object({
-    content: string().max(500).required(),
+  const schema = joi.object({
+    content: joi.string().max(500).required(),
   });
 
   return schema.validate(input);
